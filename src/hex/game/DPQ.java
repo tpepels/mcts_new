a7f74ea5a6476d178d569e135322636416a087c9
Tom Pepels <tpepels@gmail.com>
2020-01-13 15:52:19 +0100

    finished Amazons

--- src/hex/game/DPQ.java
+++ src/hex/game/DPQ.java
@@ -112,9 +112,6 @@ class Node implements Comparable<Node> {
     public int[] node;
     public int cost;
 
-    public Node() {
-    }
-
     public Node(int[] node, int cost) {
         this.node = node;
         this.cost = cost;
